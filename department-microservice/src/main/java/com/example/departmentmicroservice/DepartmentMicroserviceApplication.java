7278533124454ab05e7557c303be27a3c2290ed3
prajvals <prajvaltheking@gmail.com>
2022-09-12 10:07:35 +0530

    initial setup

--- department-microservice/src/main/java/com/example/departmentmicroservice/DepartmentMicroserviceApplication.java
+++ department-microservice/src/main/java/com/example/departmentmicroservice/DepartmentMicroserviceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DepartmentMicroserviceApplication {
 
+	//initial setup
 	public static void main(String[] args) {
 		SpringApplication.run(DepartmentMicroserviceApplication.class, args);
 	}
